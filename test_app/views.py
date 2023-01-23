083861daa014dc15ab9267c0ac74ff958f59ee02
rajathakurk <rajatthakurk.com>
2023-01-23 15:29:19 +0530

    docker_with_django

---
+++ test_app/views.py
@@ -0,0 +1,8 @@
+from rest_framework.views import APIView
+from django.http import JsonResponse
+
+class HomeView(APIView):
+    def get(self,request):
+        return JsonResponse({
+            "message":"Hello from django docker is working"
+        })
