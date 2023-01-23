392394543b014c7a1e3782f4af56a4f6a333f268
rajathakurk <rajatthakurk.com>
2023-01-23 15:46:35 +0530

    test new branch

--- test_app/views.py
+++ test_app/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from django.http import JsonResponse
-
+from django.contrib.auth.models import User
 class HomeView(APIView):
     def get(self,request):
         return JsonResponse({
